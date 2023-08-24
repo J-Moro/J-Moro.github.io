f554af353c05145fee94ddcc2341594095cbe63d
Julia Moro Nunes <moronunes.julia@gmail.com>
2023-08-24 14:31:16 -0300

    introducing JS to count pixels

---
+++ index.js
@@ -0,0 +1,7 @@
+var fontSize = 12;
+var test = document.getElementById("education");
+test.style.fontSize = fontSize;
+var height = (test.clientHeight + 1) + "px";
+var width = (test.clientWidth + 1) + "px"
+
+console.log(height, width);
